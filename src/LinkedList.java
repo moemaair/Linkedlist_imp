ccddff2d25dccd8028556bde345a5cb9c583bf61
Mohamed A. Ibrahim <rageali12@gmail.com>
2022-08-15 23:17:16 +0300

    initial commit

---
+++ src/LinkedList.java
@@ -0,0 +1,21 @@
+public class LinkedList {
+    Node head;
+
+    public void insert(int data){
+        Node n = new Node();
+        n.data = data;
+
+        if(head == null){
+            head = n;
+        }
+        else {
+            Node n2 = head;
+            while(n2.next !=null){
+
+            }
+        }
+    }
+
+
+
+}
